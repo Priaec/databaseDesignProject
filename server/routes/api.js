d2096184b626eb7e3147bd08286666c51d4bcdf4
Priaec <101358256+Priaec@users.noreply.github.com>
2023-04-22 10:49:35 -0700

    Update to Main

--- server/routes/api.js
+++ server/routes/api.js
@@ -101,13 +101,18 @@ router.post('/login', async (req, res)=>{
 
 //initalize the database for
 router.post('/init',  async (req,res)=>{
+   //run the foreign key checks query
+   con.query('SET FOREIGN_KEY_CHECKS = 0;', (err)=>{
+    if (err){
+       return res.status(500).json({message: 'Error Removing Foreign Key Oonstraints'})
+    }
+    //returns all tables in the schema
     con.query('SHOW TABLES', (err, results) => {
         if (err) {
           console.error('Error getting list of tables:', err);
           res.status(500).send('Error dropping tables');
         } else {
           const tables = results.map(result => result[`Tables_in_${con.config.database}`]);
-    
           // Drop each table in the schema
           for (const table of tables) {
             con.query(`DROP TABLE IF EXISTS \`${table}\``, err => {
@@ -121,10 +126,11 @@ router.post('/init',  async (req,res)=>{
           }
           console.log('All tables dropped successfully')
           const sql = fs.readFileSync(__dirname + '/comp440.sql').toString();
-          console.log(sql);
+          //console.log(sql);
           const queries = sql.split(';');
           queries.pop();
           queries.forEach((query)=>{
+            console.log({query: query})
             con.query(query, function (err, result) {
                 if (err)
                     return res.status(500).json({message: 'Error executing a query'});
@@ -133,7 +139,8 @@ router.post('/init',  async (req,res)=>{
         console.log('All queries completed');
         return res.status(200).json({message: 'Initialized'});
         }
-      });
+    });
+  });
 });
 
 //hash a string
@@ -141,6 +148,92 @@ hash = (str) =>{
     const hash = crypto.createHash('sha256').update(str).digest('hex');
     return hash;
 }
+
+//add a new item in the item list
+router.post('/add-item', (req, res) => {
+  const { title, description, category, price, userName } = req.body;
+  console.log({userName: userName})
+  const query = 'INSERT INTO items (title, description, category, price, username) VALUES (?, ?, ?, ?, ?)';
+  con.query(query, [title, description, category, price, userName], (err, result) => {
+    if (err) throw err;
+    //res.redirect('/?userName=${userName}&category=');
+    const url = '/api/search?category=&userName=' + encodeURIComponent(userName)
+    console.log(url)
+    res.setHeader('Cache-Control', 'no-cache');
+    setTimeout(()=>{
+      res.redirect(url);
+    }, 1000)
+  });
+});
+
+// Search items
+router.get('/search', async (req, res) => {
+  const { category, userName } = req.query;
+  let query = 'SELECT * FROM items WHERE category LIKE ?';
+  if(category == null || undefined)
+    query = 'SELECT * FROM items';
+  try{  
+    con.query(query, [`%${category}%`], (err, searchResults) => {
+      if (err) throw err;
+      res.status(200).render('drop', { searchResults , userName: userName, category: ''});
+    });
+  }
+  catch(err){
+    res.status(500).json({message: err.message});
+  }
+});
+
+// Render review page
+router.get('/review/:id', (req, res) => {
+  //id is the item id, not the review id
+  const { id } = req.params;
+  const userName = req.query.username;
+  const query = 'SELECT * FROM items WHERE id = ?';
+  con.query(query, [id], (err, result) => {
+    if (err) throw err;
+    //run a query for all the reviews associated with that id
+    const reviewQuery = 'SELECT reviews.username, rating, reviews.description FROM items JOIN reviews ON item_id = items.id and item_id = ?';
+    const item = result[0];
+    con.query(reviewQuery, [id], (err, result)=>{
+      if (err)
+        return res.status(500).json({message: 'Error sending you to review screen'});
+      //send back the reviews, including the name of the items
+      return res.render('review', {item: item, reviews: result, userName: userName});
+    })
+  });
+});
+
+// Add review
+router.post('/review/:id', (req, res) => {
+  //id is the id of the item
+  const { id } = req.params;
+  const { rating, description, username } = req.body;
+  //we need to find how many times the user has made a review for this item
+  const reviewQuery = 'SELECT COUNT(id) AS review_count FROM reviews WHERE item_id = ? GROUP BY username having username = ?';
+  con.query(reviewQuery, [id, username], (err, result)=>{
+    if(err)
+      return res.status(500).json({message: 'Error retrieving count of reviews for given user and item selected'});
+    console.log({anyResults: result});
+    const numReviews = 0;
+    if(result == [])
+      numReviews = result[0].review_count;
+    if (numReviews >= 3)
+      return res.redirect(`/api/search?userName=${username}`);
+    const query = 'INSERT INTO reviews (item_id, rating, description, username) VALUES (?, ?, ?, ?)';
+    console.log(`INSERT INTO reviews (item_id, rating, description, username) VALUES
+     (${id}, ${rating}, ${description}, ${username})`)
+    con.query(query, [id, rating, description, username], (err, result) => {
+      if (err) 
+        return res.status(500).json({message: 'Error adding review'});
+      res.redirect(`/api/search?userName=${username}`);
+    });
+  });
+});
+
+// Render index page with empty search results
+router.get('/', (req, res) => {
+    res.render('index', { searchResults: [] });
+});
 
 //create connection to root connection
 let con = mysql.createConnection({
--- server/routes/comp440.sql
+++ server/routes/comp440.sql
@@ -7,6 +7,37 @@ CREATE TABLE `user` (
 );INSERT INTO `user` (`username`, `password`, `firstName`, `lastName`, `email`) VALUES
 ('JoshuaCohen', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'Joshua', 'Cohen', 'notJoshCohen@yahoo.com'),
 ('Student1', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'student', 'person', 'student@yahoo.com'),
-('admin', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', 'admin', 'person', 'admin@yahoo.com'),
-('cassi123', 'c6be040f3fd060f9229167e545c7c7cf90507982ecb35aaf2226caab39f580e1', 'Cassidy', 'Smith', 'cassidy123@gmail.com');
+('admin', '38a81e87e79631e602bf5fbd307ce2fcd382b1670c585ea09032aac778a80531', 'admin', 'person', 'admin@yahoo.com'),
+('cassi123', 'c6be040f3fd060f9229167e545c7c7cf90507982ecb35aaf2226caab39f580e1', 'Cassidy', 'Smith', 'cassidy123@gmail.com'
+);CREATE TABLE `items` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `title` VARCHAR(255) NOT NULL,
+  `description` TEXT NOT NULL,
+  `category` VARCHAR(255) NOT NULL,
+  `price` DECIMAL(10, 2) NOT NULL
+);ALTER TABLE `items` ADD COLUMN `username` VARCHAR(50
+);ALTER TABLE `items` ADD CONSTRAINT `fk_items_username` FOREIGN KEY (`username`) REFERENCES `user`(`username`
+);CREATE TABLE `reviews` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `rating` ENUM('excellent', 'good', 'fair', 'poor') NOT NULL,
+  `description` TEXT NOT NULL,
+  `username` VARCHAR(50) NOT NULL
+);ALTER TABLE `reviews` ADD COLUMN `item_id` INT NOT NULL
+;ALTER TABLE `reviews` ADD CONSTRAINT `fk_reviews_id` FOREIGN KEY (`item_id`) REFERENCES `items`(`id`
+);ALTER TABLE `reviews` ADD CONSTRAINT `fk_reviews_username` FOREIGN KEY (`username`) REFERENCES `items`(`username`
+);INSERT INTO `items` (`title`, `description`, `category`, `price`, `username`) VALUES
+('Iphone 14', 'Produced 2023', 'Electronics', '899', 'admin'),
+('Ninja Blender', 'Wonderful for making shakes and smoothies', 'Appliances', '199.89', 'Student1'),
+('Intel core I9 12900K', 'The strongest processor on the market', 'Electronics', '799.89', 'JoshuaCohen'),
+('SurfBoard', 'Used to go surfing in the ocean', 'Outdoor', '249.79', 'admin'),
+('Large Suitcase', 'Used for domestic and international travel and color is Blue', 'Travel', '149.99', 'cassi123'
+);INSERT INTO `reviews` (`item_id`, `rating`, `description`, `username`) VALUES
+('3', 'fair', 'These processors have issues with overclocking. Also quite a high price.', 'admin'),
+('1', 'good', 'Great quality, however, it is a high price.', 'Student1'),
+('4', 'excellent', 'Durable and has lasted a long time.', 'admin'),
+('2', 'poor', 'Broke on me within the first 2 days. Quite upsetting!', 'JoshuaCohen'),
+('5', 'excellent', 'Durable and has lasted a long time. Very affordable!', 'cassi123'
+);SET FOREIGN_KEY_CHECKS = 0
+;
+
 
